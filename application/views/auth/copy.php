98f91122efb4ffbfae3c324e9756458b4a3e2c65
monsidev <rsheremetov@list.ru>
2018-04-09 17:59:42 +0300

    View Edit

--- application/views/auth/copy.php
+++ application/views/auth/copy.php
@@ -3,7 +3,7 @@
     <a href="/about" class="auth-copy__link">О Компании</a>
     <a href="/rules" class="auth-copy__link">Правила</a>
     <a href="/prod" class="auth-copy__link">Продукты</a>
-    <a href="/post" class="auth-copy__link">Публикации</a>
+    <a href="/cases" class="auth-copy__link">Кейсы</a>
   </div>
   <div class="auth-copy__text">Arida Inc. © <?= date('Y') ?></div>
 </div>
