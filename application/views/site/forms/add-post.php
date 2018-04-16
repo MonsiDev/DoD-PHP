f4dc6c6b5968a2c979b1479e8ce3831dfcfb4d6e
monsidev <rsheremetov@list.ru>
2018-04-16 18:02:03 +0300

    User Profile

--- application/views/site/forms/add-post.php
+++ application/views/site/forms/add-post.php
@@ -1,4 +1,28 @@
 <div class="add-post">
-  sdfsdf
-
+  <textarea name="name" class="add-post__text" placeholder="Что нового?"></textarea>
+  <div class="add-post__bar">
+    <div class="add-post__buttons">
+      <button class="add-post__button">
+        <svg class="svg-icon svg-icon--cog">
+          <use xlink:href="/assets/img/icons-pack.svg#svg-icon-cog"></use>
+        </svg>
+      </button>
+      <button class="add-post__button">
+        <svg class="svg-icon svg-icon--photo">
+          <use xlink:href="/assets/img/icons-pack.svg#svg-icon-photo"></use>
+        </svg>
+      </button>
+      <button class="add-post__button add-post__button">
+        <svg class="svg-icon svg-icon--video">
+          <use xlink:href="/assets/img/icons-pack.svg#svg-icon-video"></use>
+        </svg>
+      </button>
+      <button class="add-post__button">
+        <svg class="svg-icon svg-icon--text">
+          <use xlink:href="/assets/img/icons-pack.svg#svg-icon-text"></use>
+        </svg>
+      </button>
+    </div>
+    <button class="add-post__save">Добавить</button>
+  </div>
 </div>
--- assets/js/build.js
+++ assets/js/build.js
@@ -39,59 +39,83 @@
   window['Ajax'] = ajax;
 }));
 
-;(function(){
-  'use strict';
+(function() {
+  "use strict";
 
   var i = 0;
 
   window.addEventListener("load", function(_e) {
-    document.getElementById('auth-btn').addEventListener("click", function(_e) {
-      var indexOf = [].indexOf.call(this.children, (_e ? _e.target : event.srcElement) );
-      var callTab = document.getElementById(_e.target.getAttribute("data-tab-id"));
-      var oldTab =  document.getElementById(this.children[ (indexOf - 1) * (indexOf - 1) ].getAttribute("data-tab-id"));
+    var authBtn = document.getElementById("auth-btn");
+    if (authBtn) {
+      authBtn.addEventListener("click", function(_e) {
+        var indexOf = [].indexOf.call(
+          this.children,
+          _e ? _e.target : event.srcElement
+        );
+        var callTab = document.getElementById(
+          _e.target.getAttribute("data-tab-id")
+        );
+        var oldTab = document.getElementById(
+          this.children[(indexOf - 1) * (indexOf - 1)].getAttribute(
+            "data-tab-id"
+          )
+        );
 
-      _e.target.classList.add("active");
-      this.children[ (indexOf - 1) * (indexOf - 1) ].classList.remove("active");
+        _e.target.classList.add("active");
+        this.children[(indexOf - 1) * (indexOf - 1)].classList.remove("active");
 
-      callTab.removeAttribute("hidden");
-      oldTab.setAttribute("hidden", true);
-    });
-    document.querySelectorAll('.auth-tab__input input').forEach(function(_each) {
-      _each.addEventListener('focus', function(_e) {
-        if(this.parentNode.classList.contains('error')) {
-          this.parentNode.classList.remove('error');
-        }
+        callTab.removeAttribute("hidden");
+        oldTab.setAttribute("hidden", true);
       });
-      _each.addEventListener('blur', function(_e) {
-        if(this.value == '' ||
-          (this.getAttribute('name') == 'email' &&
-          /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/.test(this.value) == false)) {
-          this.parentNode.classList.add('error');
-        }
+    }
+    document
+      .querySelectorAll(".auth-tab__input input")
+      .forEach(function(_each) {
+        _each.addEventListener("focus", function(_e) {
+          if (this.parentNode.classList.contains("error")) {
+            this.parentNode.classList.remove("error");
+          }
+        });
+        _each.addEventListener("blur", function(_e) {
+          if (
+            this.value == "" ||
+            (this.getAttribute("name") == "email" &&
+              /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/.test(
+                this.value
+              ) == false)
+          ) {
+            this.parentNode.classList.add("error");
+          }
+        });
       });
-    });
     function validForm(_e) {
-      var msgText = this.querySelector('#msg-error');
+      var msgText = this.querySelector("#msg-error");
       var $This = this;
-      var postData = '';
-      this.querySelectorAll('input').forEach(function(_each) {
-        postData += _each.getAttribute('name') + '=' + _each.value + '&';
+      var postData = "";
+      this.querySelectorAll("input").forEach(function(_each) {
+        postData += _each.getAttribute("name") + "=" + _each.value + "&";
       });
       Ajax({
-        type: 'POST',
-        url: '/login',
-        data: postData.replace(/&+$/,''),
+        type: "POST",
+        url: "/login",
+        data: postData.replace(/&+$/, ""),
         onSuccess: function(json) {
           json = JSON.parse(json);
-          if(json.status == 'error') {
+          if (json.status == "error") {
             msgText.innerHTML = json.msg;
-            for(var field in json.fields) {
-              if(json.fields[field] == 'error') {
-                if(/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/.test($This.querySelector('input[name="email"]').value) == false
-                  && $This.querySelector('input[name="email"]').value != '') {
-                  msgText.innerHTML = 'Не корректный email';
+            for (var field in json.fields) {
+              if (json.fields[field] == "error") {
+                if (
+                  /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/.test(
+                    $This.querySelector('input[name="email"]').value
+                  ) == false &&
+                  $This.querySelector('input[name="email"]').value != ""
+                ) {
+                  msgText.innerHTML = "Не корректный email";
                 }
-                $This.querySelector('input[name="' + field + '"]').parentNode.classList.add('error');
+                $This
+                  .querySelector('input[name="' + field + '"]')
+                  .parentNode.classList.add("error");
               }
             }
           } else {
@@ -99,14 +123,21 @@
           }
         },
         onError: function(data) {
-          msgText.innerHTML = '<p>Произошла внутренняя ошибка!</p><p>Попробуйте ещё раз</p>';
+          msgText.innerHTML =
+            "<p>Произошла внутренняя ошибка!</p><p>Попробуйте ещё раз</p>";
         }
       });
       _e.preventDefault();
     }
 
-    document.getElementById('form-reg').addEventListener('submit', validForm);
-    document.getElementById('form-auth').addEventListener('submit', validForm);
+    var formReg = document.getElementById("form-reg");
+    var formAuth = document.getElementById("form-auth");
+    if(formReg) {
+      formReg.addEventListener("submit", validForm);
+    }
+    if(formAuth) {
+      formAuth.addEventListener("submit", validForm);
+    }
   });
 })();
 
